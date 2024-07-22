f80411803d5c849dd30b33ddd2f1aff0c2a921a8
wild_duck <wezonaser50@gmail.com>
2024-07-22 06:50:37 +0300

    chore:ignored

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,12 @@ export const app = express()
 app.use(bodyParser.json({ limit: '40mb' }))
 app.use(bodyParser.urlencoded({ limit: '40mb', extended: false }))
 app.use(
-  cors({
-    origin: 'https://upload-thing-theta.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true
-  })
+    cors({
+        origin: config.corsOrigin,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+        optionsSuccessStatus: 204
+    })
 )
 console.log(config.corsOrigin)
 
@@ -23,7 +23,7 @@ app.use(blobRoutes)
 app.use(authRouter)
 
 app.listen(config.port, () =>
-  console.log(`server is running on port: ${config.port}`)
+    console.log(`server is running on port: ${config.port}`)
 )
 
 export default app
